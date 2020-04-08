e207f5bbb43f2839ff3eef931fc0c26128534aec
Nicolas <nicolascardoso99@gmail.com>
2020-04-08 18:14:28 +0200

    Légère modif affichage

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Projet de théorie des graphes S6\n\n\n");
+		System.out.println("Projet de théorie des graphes S6\n");
 		
 		Graph graph = new Graph("graph1.txt");
 		graph.afficher_graph();
